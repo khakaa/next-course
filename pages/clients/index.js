8bc0cf76c18cb229caac5fc82ceeab0dd4b9ad94
khakaa <phr0210@naver.com>
2023-08-01 16:30:14 +0900

    Link 컴포넌트로 내비게이팅

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function ClientPage() {
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        <li>
+          <Link href="/clients/max">Maximilian</Link>
+        </li>
+        <li>
+          <Link href="/clients/menu">Meneul</Link>
+        </li>
+      </ul>
     </div>
   );
 }
