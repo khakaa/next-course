1ae7454fa5cbcb9d0b51c99d1d5fdd1f6f383a4a
khakaa <phr0210@naver.com>
2023-08-02 17:03:41 +0900

    getStaticProps option - redirect, notFound

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,21 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  // getStaticProps option
+  // 1.redirect
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  // 2. 404 페이지
+  if (data.products.length === 0) {
+    return { noFound: true };
+  }
+
   return {
     props: {
       products: data.products,
