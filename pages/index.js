fa70ea44296657b143a2ff3b2cfa030a2a0ff6de
khakaa <phr0210@naver.com>
2023-08-02 16:25:17 +0900

    filesystem 사용하여 서버 사이드로 데이터 불러오기

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import path from "path";
+import fs from "fs/promises";
+
 function HomePage(props) {
   const { products } = props;
   return (
@@ -10,9 +13,13 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // 전체 프로젝트 디렉토리에서 시작하여 data/dummy-backend.json 경로 지정해줌
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
     },
   };
 }
