01922ca332d6cd057ebdaac7227db436f6e0eb08
khakaa <phr0210@naver.com>
2023-08-03 14:29:14 +0900

    getStaticPaths - paths 배열 pathsWithParams 변수로 선언하여 지정

--- pages/[pid].js
+++ pages/[pid].js
@@ -15,26 +15,40 @@ function ProductDetailPage({ loadedProduct }) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-
-  const productId = params.pid;
-  console.log(productId);
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const data = await getData();
   const productone = data.products.find((product) => product.id === productId);
 
   return { props: { loadedProduct: productone } };
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const pathsWithParams = data.products.map((product) => ({
+    params: {
+      pid: product.id,
+    },
+  }));
+
+  console.log(ids);
   return {
-    paths: [{ params: { pid: "p1" } }], // /p1 페이지만 빌드타임에 생성
+    paths: pathsWithParams, // /p1 페이지만 빌드타임에 생성
     // fallback: true, // 나머지 페이지는 빌드 타임에 생성 x
-    fallback: "blocking", // 나머지 페이지는 fallback상태를 보여주지 않고, SSR처럼 동작
-    // fallback: false,
+    // fallback: "blocking", // 나머지 페이지는 fallback상태를 보여주지 않고, SSR처럼 동작
+    fallback: false,
   };
 }
 export default ProductDetailPage;
