83aa30efd55ce84305e4ae035713279a91c694d4
khakaa <phr0210@naver.com>
2023-08-03 23:26:34 +0900

    Added Home page markup

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 function HomePage(props) {
   return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
+    <div>
+      <h1>The Home Page</h1>
+    </div>
   );
 }
 
