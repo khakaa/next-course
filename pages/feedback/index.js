2bce10dbc83ad4a88b7990aeba584c04ed780d00
khakaa <phr0210@naver.com>
2023-08-04 15:31:57 +0900

    Added Dynamic api route

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 export default function FeedbackPage({ feedbackItems }) {
+  const [feedbackOne, setFeedbackOne] = useState();
+
+  function loadFeedbackOneHandler(id) {
+    fetch(`/api/${id}`)
+      .then((res) => res.json())
+      .then((data) => setFeedbackOne(data.feedback));
+  }
   return (
-    <ul>
-      {feedbackItems.map((item) => (
-        <li key={item.id}>{item.feedbackText}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackOne && <p>{feedbackOne.email}</p>}
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.feedbackText}
+            <button onClick={loadFeedbackOneHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
